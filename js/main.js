8aeb1578302d84ef163d4072c043814b1c917538
zemk <shapoval.44105@gmail.com>
2021-09-08 16:15:12 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+  // effect: 'cube',
+
+  
+});
